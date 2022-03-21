aef9e9d6d07c24fa75b85303c525b6d96a65acd6
ctes930013 <a0984108310@gmail.com>
2022-03-21 18:19:30 +0800

    refactor:將provider改為全局 feat:引入multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,22 @@
  * Copyright (c) 2019 Yakka, LLC. All rights reserved.
  * See LICENSE for distribution and usage details.
  */
+import 'package:flutterwebview/config/web_provider.dart';
+import 'package:provider/provider.dart';
+
 import 'components/app/app_component.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(AppComponent());
+  runApp(
+    //引入provider
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<WebProvider>(
+            create: (context) => WebProvider(false),
+          ),
+        ],
+        child: AppComponent(),
+      ),
+  );
 }
