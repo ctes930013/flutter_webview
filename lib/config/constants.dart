6e0d976073225a9bc8b890b1332844b9a1ea9747
ctes930013 <a0984108310@gmail.com>
2022-03-24 10:57:37 +0800

    feat:新增多國語言的偏好儲存

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,4 +1,8 @@
 class Constants{
 
+  //定義首頁底部選單的圖像大小
   static const double bottomNavImgHeight = 30.0;
+
+  //定義語系的偏好key
+  static const String languageKey = "language";
 }
--- lib/providers/language_provider.dart
+++ lib/providers/language_provider.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutterwebview/config/constants.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 //多國語系的provider
 class LanguageProvider extends ChangeNotifier{
   //紀錄語系
   Locale locale = const Locale('zh', 'TW');
 
+  //取得儲存於偏好的語系
+  Future<String> getLocalePref() async {
+    //從偏好中讀取
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String local = prefs.getString(Constants.languageKey) ?? "";
+    return local;
+  }
+
   Locale get getLocale  => locale;
 
   LanguageProvider();
 
-  setLocale(locale){
+  setLocale(locale) async{
     this.locale = locale;
+    //存入偏好
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString(Constants.languageKey, locale.toString());
     notifyListeners();
   }
 }
