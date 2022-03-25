0fd6af5cb23b64336e67d38e3ea6791a7d36fcef
joss <sujiaxu0202@gmail.com>
2022-03-25 13:27:35 +0800

    create file for buttons data

---
+++ lib/components/data/home_upper_button_data.dart
@@ -0,0 +1,14 @@
+import '../../models/ui/models/home_upper_button_section_data.dart';
+
+class HomeUpperButtonData {
+  final List<HomeUpperButtonSectionData> buttonList = [
+    HomeUpperButtonSectionData(text: '推薦', id: 0),
+    HomeUpperButtonSectionData(text: '星秀', id: 1),
+    HomeUpperButtonSectionData(text: '顏值', id: 2),
+    HomeUpperButtonSectionData(text: '新人', id: 3),
+    HomeUpperButtonSectionData(text: '舞蹈', id: 4),
+    HomeUpperButtonSectionData(text: '交友', id: 5),
+    HomeUpperButtonSectionData(text: '音樂', id: 6),
+    HomeUpperButtonSectionData(text: '女神', id: 7)
+  ];
+}
