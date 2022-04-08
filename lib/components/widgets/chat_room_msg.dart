ce91891349b151864e8a018f3746c68f095308ad
ctes930013 <a0984108310@gmail.com>
2022-04-08 17:42:51 +0800

    feat:新增聊天室文字動畫

--- lib/components/widgets/chat_room_msg.dart
+++ lib/components/widgets/chat_room_msg.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:show_up_animation/show_up_animation.dart';
 
 import '../../models/models/chat_room_msg_model.dart';
 
@@ -13,22 +14,27 @@ class ChatRoomMsg extends StatefulWidget {
 class _ChatRoomMsgState extends State<ChatRoomMsg> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(3),
-      height: 24,
-      child: Row(
-        children: <Widget>[
-          if (widget.msgListItem.img != 'lib/images/')
-            Image.asset(widget.msgListItem.img),
-          Text(
-            widget.msgListItem.name,
-            style: const TextStyle(color: Color.fromARGB(255, 102, 171, 228)),
-          ),
-          Text(
-            widget.msgListItem.text,
-            style: const TextStyle(color: Colors.white),
-          )
-        ],
+    return ShowUpAnimation(
+      animationDuration: const Duration(seconds: 1),
+      curve: Curves.bounceIn,
+      direction: Direction.vertical,
+      child: Container(
+        padding: const EdgeInsets.all(3),
+        height: 24,
+        child: Row(
+          children: <Widget>[
+            if (widget.msgListItem.img != 'lib/images/')
+              Image.asset(widget.msgListItem.img),
+            Text(
+              widget.msgListItem.name,
+              style: const TextStyle(color: Color.fromARGB(255, 102, 171, 228)),
+            ),
+            Text(
+              widget.msgListItem.text,
+              style: const TextStyle(color: Colors.white),
+            )
+          ],
+        ),
       ),
     );
   }
